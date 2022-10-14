8f791083ba8c4d08e739fa9d52978a8848fe6953
Lauren Ashley <la.zaubi@gmail.com>
2022-10-14 14:28:20 -0700

    progress adding user input handling to play.js

--- play.js
+++ play.js
@@ -2,5 +2,21 @@
 
 const connect = require('./client');
 
+const setupInput = () => {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = data => {
+  // Check for ctrl + c input to terminate the game
+};
+
 console.log("Connecting ...");
 connect.connect();
+
+setupInput();
