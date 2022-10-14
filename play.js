be807356a6361e76065c719d352dafa68b667a63
Lauren Ashley <la.zaubi@gmail.com>
2022-10-14 13:25:14 -0700

    connected to server and receive data from server

--- play.js
+++ play.js
@@ -1,1 +1,21 @@
+// Client
+const net = require("net");
 
+// establishes a connection with the game server
+const connect = () => {
+  const conn = net.createConnection({
+    host: 'localhost',
+    port: 50541
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on('data', data => {
+    console.log('Message from the server: ', data);
+  });
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
