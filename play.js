824b021b87c2bfd792ea4a62aadf3f1677928e64
Lauren Ashley <la.zaubi@gmail.com>
2022-10-15 12:45:03 -0700

    using stdin now, added data listener and handling for user ctrl+c to exit process

--- play.js
+++ play.js
@@ -1,19 +1,21 @@
 // play.js
-
+const process = require('node:process');
 const connect = require('./client');
 
-const setupInput = () => {
+const setupInput = function () {
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
-
   stdin.on("data", handleUserInput);
   return stdin;
 };
 
-const handleUserInput = data => {
-  // Check for ctrl + c input to terminate the game
+// When resuming stdin (as done above in setupInput) there needs to be  way for user to exit process
+const handleUserInput = (data) => {
+  if (data === '\u0003') {
+    process.exit();
+  }
 };
 
 console.log("Connecting ...");
